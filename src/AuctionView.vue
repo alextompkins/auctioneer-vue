7b20b2a3c6ff8a1d4e6c35637597bcee8cf5c364
Alex Tompkins <alextompkins3@gmail.com>
2018-05-20 22:56:34 +1200

    Refactor AuctionView to split out BidPanel - this deserves its own component.
    Took 17 minutes

--- src/AuctionView.vue
+++ src/AuctionView.vue
@@ -26,23 +26,7 @@
 
       <b-col xs="12" md="4">
         <h4>Bids</h4>
-        <b-list-group>
-          <auction-status-box :auction="auction"/>
-          <b-list-group-item v-if="cannotBidReason">
-            <h5>Cannot bid</h5>
-            {{ cannotBidReason }}
-          </b-list-group-item>
-          <new-bid-box :session="session"
-                       :auctionId="$route.params.id"
-                       :minNextBid="minNextBid"
-                       :bids="auction.bids"
-                       v-else/>
-          <bid-list-item
-            v-for="bid in bidsSortedByMostRecent"
-            :bid="bid"
-            :key="bid.datetime">
-          </bid-list-item>
-        </b-list-group>
+        <bid-panel :session="session" :auction="auction"/>
       </b-col>
 
     </b-row>
@@ -51,17 +35,11 @@
 </template>
 
 <script>
-  import AuctionStatusBox from "./AuctionStatusBox";
-  import BidListItem from "./BidListItem";
-  import NewBidBox from "./NewBidBox";
   import {formatDateTimeAbsolute} from "./helpers";
+  import BidPanel from "./BidPanel";
 
   export default {
-    components: {
-      AuctionStatusBox,
-      NewBidBox,
-      BidListItem
-    },
+    components: {BidPanel},
     name: "auction-view",
     props: ['session'],
 
@@ -88,29 +66,6 @@
           return "Ended " + dateTimeString;
         }
       },
-      minNextBid: function () {
-        return Math.max(this.auction.startingBid, this.auction.currentBid + 1);
-      },
-      cannotBidReason: function () {
-        if (this.session.loggedIn === false) {
-          return "You must login to be able to bid.";
-        } else if (this.auction.seller.id === this.session.user.id) {
-          return "You cannot bid on your own auction.";
-        } else if (Date.now() < this.auction.startDateTime) {
-          return "Bidding on this auction has not yet begun.";
-        } else if (Date.now() >= this.auction.endDateTime) {
-          return "This auction has ended.";
-        } else {
-          return "";
-        }
-      },
-      bidsSortedByMostRecent: function () {
-        return this.auction.bids.sort((b1, b2) => b1.datetime < b2.datetime);
-      }
-    },
-
-    mounted: function () {
-      this.getAuction();
     },
 
     methods: {
@@ -123,6 +78,10 @@
             // TODO deal with error
           });
       }
+    },
+
+    mounted: function () {
+      this.getAuction();
     }
 
   }
---
+++ src/BidPanel.vue
@@ -0,0 +1,61 @@
+<template>
+  <b-list-group>
+    <auction-status-box :auction="auction"/>
+    <b-list-group-item v-if="cannotBidReason">
+      <h5>Cannot bid</h5>
+      {{ cannotBidReason }}
+    </b-list-group-item>
+    <new-bid-box :session="session"
+                 :auctionId="$route.params.id"
+                 :minNextBid="minNextBid"
+                 :bids="auction.bids"
+                 v-else/>
+    <bid-list-item
+      v-for="bid in bidsSortedByMostRecent"
+      :bid="bid"
+      :key="bid.datetime">
+    </bid-list-item>
+  </b-list-group>
+</template>
+
+<script>
+  import AuctionStatusBox from "./AuctionStatusBox";
+  import BidListItem from "./BidListItem";
+  import NewBidBox from "./NewBidBox";
+
+  export default {
+    components: {
+      AuctionStatusBox,
+      NewBidBox,
+      BidListItem
+    },
+    name: "bid-panel",
+    props: ['session', 'auction'],
+
+    computed: {
+      minNextBid: function () {
+        return Math.max(this.auction.startingBid, this.auction.currentBid + 1);
+      },
+      cannotBidReason: function () {
+        if (this.session.loggedIn === false) {
+          return "You must login to be able to bid.";
+        } else if (this.auction.seller.id === this.session.user.id) {
+          return "You cannot bid on your own auction.";
+        } else if (Date.now() < this.auction.startDateTime) {
+          return "Bidding on this auction has not yet begun.";
+        } else if (Date.now() >= this.auction.endDateTime) {
+          return "This auction has ended.";
+        } else {
+          return "";
+        }
+      },
+      bidsSortedByMostRecent: function () {
+        return this.auction.bids.sort((b1, b2) => b1.datetime < b2.datetime);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
