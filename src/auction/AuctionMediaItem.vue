7925e0db4a192292c259e44ac19100371311fe36
Alex Tompkins <alextompkins3@gmail.com>
2018-05-23 23:24:06 +1200

    Improve AuctionMediaItems, perhaps slightly overcomplicated.
    Took 27 minutes

--- src/auction/AuctionMediaItem.vue
+++ src/auction/AuctionMediaItem.vue
@@ -1,14 +1,19 @@
 <template>
   <b-media class="mb-1">
     <b-img slot="aside" :src="imgPath()" width="100" alt="placeholder" ></b-img>
+
     <b-link :to="{ name: 'auction-view', params: { id: auction.id } }">
       <h5 class="mt-0">{{ auction.title }}</h5>
     </b-link>
+
     ({{ auction.categoryTitle }})
-    <p>
-      Current bid is {{ currentBidAsCurrency }}
-      <br>Auction ends on {{ endDateString }}.
-    </p>
+
+    <div>{{ bidString }}</div>
+
+    <div v-if="hasEnded">Ended {{ endDateString }}</div>
+    <div v-else-if="hasStarted">Will end {{ endDateString }}</div>
+    <div v-else>Will begin {{ startDateString }}</div>
+
   </b-media>
 </template>
 
@@ -20,11 +25,43 @@
     props: ['auction'],
 
     computed: {
+      hasStarted: function() {
+        return Date.now() >= this.auction.startDateTime;
+      },
+      hasEnded: function () {
+        return Date.now() >= this.auction.endDateTime;
+      },
+      hasBids: function () {
+        return this.auction.currentBid !== 0;
+      },
+      startDateString: function () {
+        return formatDateTimeAbsolute(this.auction.startDateTime);
+      },
       endDateString: function () {
         return formatDateTimeAbsolute(this.auction.endDateTime)
       },
       currentBidAsCurrency: function () {
         return amountAsCurrency(this.auction.currentBid);
+      },
+      startingBidAsCurrency: function () {
+        return amountAsCurrency(this.auction.startingBid);
+      },
+      bidString: function () {
+        if (this.hasEnded) {
+          if (this.hasBids) {
+            return "Winning bid was " + this.currentBidAsCurrency + ".";
+          } else {
+            return "This auction expired without any bids.";
+          }
+        } else if (this.hasStarted) {
+          if (this.hasBids) {
+            return "Current bid is " + this.currentBidAsCurrency + ".";
+          } else {
+            return "No bids on this auction yet.";
+          }
+        } else {
+          return "This auction has not yet begun.";
+        }
       }
     },
 
@@ -39,6 +76,7 @@
 <style scoped>
   h5 {
     display: inline-block;
+    margin-bottom: 0;
   }
   img {
     max-height: 100px;
